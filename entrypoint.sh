abeff1c83abee5b035c528c3106611cb21d5708e
Justin Yoo <justin.yoojh@gmail.com>
2020-03-04 00:24:28 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,14 @@
 #!/bin/sh -l
+
+cd src
+
+dotnet restore .
+dotnet build .
+
+NAME=GitHub
+
+GREETING=$(dotnet run -p GitHubActions.ConsoleApp -- "$NAME")
+
+echo $GREETING
+
+echo ::set-output greeting=$GREETING
