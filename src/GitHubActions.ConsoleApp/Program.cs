d257a933fac90b929990cc7012d15601b12c4955
Justin Yoo <justin.yoojh@gmail.com>
2020-03-03 22:04:24 +0900

    Add .NET console app

---
+++ src/GitHubActions.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubActions.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello, {args[0]}!");
+        }
+    }
+}
